1694579cefa6895aa5f8976d83b406eebfb745b8
jlt10 <jamietan221@gmail.com>
2020-12-16 15:11:15 -0600

    Updated fano factor calculations

--- scripts/ArticleBurstiness.py
+++ scripts/ArticleBurstiness.py
@@ -1,60 +1,78 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from MultipleNetworkCollection import collect_tweet_network
+# from MultipleArticleFeatureCollection import *
 from scipy.signal import find_peaks
 import numpy as np
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-def get_fano_factor(signal):
-    return  np.var(signal) / np.mean(signal, dtype=np.float64)
+#
+# # def get_fano_factor(signal):
+# #     return  np.var(signal) / np.mean(signal, dtype=np.float64)
+#
+
 
 def remove_n_outliers(n, arr):
-    arr.sort() 
-    return arr[-n:] 
+    arr.sort()
+    print(arr[-n:])
+    return arr[:-n]
 
-# read 2020 data
-article_ids = []
-with open(f"../data/2020_article_ids.csv", "r") as f:
-    for aid in f.readlines():
-        article_ids.append([int(aid.rstrip('\n'))])
-        
-## collect network
-claim_freq = []
-fact_freq = []
+# # read 2020 data
+# article_ids = []
+# with open(f"../data/2020_article_ids.csv", "r") as f:
+#     for aid in f.readlines():
+#         article_ids.append([int(aid.rstrip('\n'))])
+#
+# ## collect network
+# claim_freq = []
+# fact_freq = []
+#
+# ## separate by type
+# for aid in article_ids:
+#     article_network_df, _ = collect_tweet_network(aid)
+#     article_network_df["date"] = article_network_df.created_at.apply(lambda dt: dt.date)
+#     if article_network_df['site_type'][0] == 'claim':
+#         claim_freq.extend(article_network_df['date'].value_counts().sort_index().to_numpy())
+#     elif article_network_df['site_type'][0] == 'fact_checking':
+#         fact_freq.extend(article_network_df['date'].value_counts().sort_index().to_numpy())
+#
+#
+# # check aggregate values
+# print("Correct claim agg: ", get_fano_factor(claim_freq))
+# print("Correct fact agg: ", get_fano_factor(fact_freq))
+#
+#
+# print()
+# print()
+#
+# claim_x, claim_y , fact_x, fact_y = [], [], [], []
+#
+# for aid in article_ids:
+#     article_network_df, _ = collect_tweet_network(aid)
+#     article_network_df["date"] = article_network_df.created_at.apply(lambda dt: dt.date)
+#     article_freq = article_network_df['date'].value_counts().sort_index().to_numpy()
+#     fano = get_fano_factor(article_freq)
+#     if article_network_df['site_type'][0] == 'claim':
+#         claim_x.append(aid[0])
+#         claim_y.append(fano)
+#     elif article_network_df['site_type'][0] == 'fact_checking':
+#         fact_x.append(aid[0])
+#         fact_y.append(fano)
+# ## rmove outliers from claim_y
+# claim_y = remove_n_outliers(3, claim_y)
 
-## separate by type
-for aid in article_ids:
-    article_network_df, _ = collect_tweet_network(aid)
-    article_network_df["date"] = article_network_df.created_at.apply(lambda dt: dt.date)
-    if article_network_df['site_type'][0] == 'claim':
-        claim_freq.extend(article_network_df['date'].value_counts().sort_index().to_numpy())
-    elif article_network_df['site_type'][0] == 'fact_checking':
-        fact_freq.extend(article_network_df['date'].value_counts().sort_index().to_numpy())
+data_path = "../data"
+article_feature_df = pd.read_csv(
+    f"{data_path}/article_features.csv",
+    parse_dates=["date_published"],
+).iloc[:, 1:].drop_duplicates(["id"])
+article_feature_df.set_index("id")
+claim_df = article_feature_df[article_feature_df.site_type == "claim"]
+fact_df = article_feature_df[article_feature_df.site_type == "fact_checking"]
 
-
-# check aggregate values        
-print("Correct claim agg: ", get_fano_factor(claim_freq))
-print("Correct fact agg: ", get_fano_factor(fact_freq))
-
-        
-print()
-print()
-
-claim_x, claim_y , fact_x, fact_y = [], [], [], []
-
-for aid in article_ids:
-    article_network_df, _ = collect_tweet_network(aid)
-    article_network_df["date"] = article_network_df.created_at.apply(lambda dt: dt.date)
-    article_freq = article_network_df['date'].value_counts().sort_index().to_numpy()
-    fano = get_fano_factor(article_freq)
-    if article_network_df['site_type'][0] == 'claim':
-        claim_x.append(aid[0])
-        claim_y.append(fano)
-    elif article_network_df['site_type'][0] == 'fact_checking':
-        fact_x.append(aid[0])
-        fact_y.append(fano)
-## rmove outliers from claim_y
-claim_y = remove_n_outliers(3, claim_y)
-
+claim_y = claim_df.fano_factor.to_numpy()
+claim_y = remove_n_outliers(5, claim_y)
+fact_y = fact_df.fano_factor.to_numpy()
 
 
 # print(claim_x, claim_y)
@@ -85,23 +103,24 @@ claim_y = remove_n_outliers(3, claim_y)
 # plt.savefig("Claim articles (id) vs burstiness (Fano Factor)")
 # plt.clf()
 
-plt.clf()
-fig, ax = plt.subplots()
-plt.title(" Fact Checking Article vs Fano factor")
+# plt.clf()
+# fig, ax = plt.subplots()
+# plt.title(" Fact Checking Article vs Fano factor")
+#
+#
+# boxplot_dict = {'Fact Checking': fact_y}
+# ax.boxplot(boxplot_dict.values())
+# ax.set_xticklabels(boxplot_dict.keys())
+#
+# plt.xlabel("Article Type ")
+# plt.ylabel("Fano factor")
+# # plt.savefig("Both articles: Spread of burstiness (Fano Factor)")
+# plt.show()
 
 
-boxplot_dict = {'Fact Checking': fact_y}
-ax.boxplot(boxplot_dict.values())
-ax.set_xticklabels(boxplot_dict.keys())
-
-plt.xlabel("Article Type ")
-plt.ylabel("Fano factor")
-plt.savefig("Both articles: Spread of burstiness (Fano Factor)")
 
 
-
-
-plt.clf()
+# plt.clf()
 fig, ax = plt.subplots()
 plt.title(" Article Type vs Fano factor")
 
@@ -113,6 +132,7 @@ ax.set_xticklabels(boxplot_dict.keys())
 plt.xlabel("Article Type ")
 #plt.legend([p1, p2], ["Claim Articles", 'Fact checking Articlesgit '])
 plt.ylabel("Fano factor")
-plt.savefig("Fact Checking: Spread of burstiness (Fano Factor)")
+# plt.savefig("Fact Checking: Spread of burstiness (Fano Factor)")
+plt.show()
 plt.clf()
 
--- scripts/MultipleArticleFeatureCollection.py
+++ scripts/MultipleArticleFeatureCollection.py
@@ -3,10 +3,11 @@ import pandas as pd
 from datetime import date, datetime, timedelta
 import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta
-from MultipleNetworkCollection import collect_tweet_network
 from scipy.signal import find_peaks
 import pytz
 from collections import defaultdict
+
+from scripts.MultipleNetworkCollection import collect_tweet_network
 
 data_path = "../data"
 features = {
@@ -87,7 +88,7 @@ def get_signal_peaks(aid, x, min_peak=50, graph=True):
         save_signal_graph(aid, x, peaks)
     return peaks
 
-# time_slice: amount of samples between peaks, decided in 
+# time_slice: amount of samples between peaks, decided in
 # in get_signal_peaks()
 def get_exp_rates(aid_bursty_map, peaks, signal_df, x, aid, time_slice=7):
     x_data = np.array(range(time_slice+1))
@@ -98,41 +99,41 @@ def get_exp_rates(aid_bursty_map, peaks, signal_df, x, aid, time_slice=7):
             ## Add small epsilon.
             y_data = np.array(x[peak_index-time_slice:peak_index+1]) + 1e-7
             log_y_data = np.log(y_data)
-            
+
             # y = C * e^(rt) => C = e^curve_fit[1], r = e^curve_fit[0]
             curve_fit = np.exp(np.polyfit(x_data, log_y_data, 1))
             ## rise
             bursty_metric += curve_fit[0]
-            
+
         if peak_index + time_slice+1 < len(x):
             y_data = np.array(x[peak_index:peak_index+time_slice+1]) + 1e-7
             log_y_data = np.log(y_data)
-            
+
             # y = C * e^(rt) => C = e^curve_fit[1], r = e^curve_fit[0]
             curve_fit = np.exp(np.polyfit(x_data, log_y_data, 1))
             ## decay
             bursty_metric += curve_fit[0]
 #         print(date, bursty_metric)
         aid_bursty_map[aid].append(bursty_metric)
-    return aid_bursty_map     
+    return aid_bursty_map
 
 def remove_n_outliers(n, arr):
-    arr.sort() 
-    return arr[-n:] 
+    arr.sort()
+    return arr[-n:]
 
 def save_rates_graph(aid_bursty_map):
     fact_y, claim_y = [], []
     for aid in aid_bursty_map:
         val = sum(aid_bursty_map[aid])/ len(aid_bursty_map[aid])
         if val == 0 or val == 0.0:
-            continue 
+            continue
         article_network_df, _ = collect_tweet_network([aid])
         if article_network_df['site_type'][0] == 'claim':
             claim_y.append(val)
         elif article_network_df['site_type'][0] == 'fact_checking':
             fact_y.append(val)
     ## Remove 3 outliers, these 3 heavily skew the graph to the extent that fact checking data is compressed to one line
-    claim_y = remove_n_outliers(3, claim_y) 
+    claim_y = remove_n_outliers(3, claim_y)
     plt.clf()
     fig, ax = plt.subplots()
     plt.title(" Article Type vs Contrived metric of burstiness.")
@@ -147,9 +148,6 @@ def save_rates_graph(aid_bursty_map):
     plt.clf()
 
 
-
-
-
 def save_signal_graph(aid, x, peaks):
     plt.plot(x)
     plt.plot(peaks, x[peaks], "x")
@@ -159,14 +157,19 @@ def save_signal_graph(aid, x, peaks):
     plt.clf()
 
 
-def get_avg_peak_distance(peaks):
+def get_peak_distance(peaks):
     n = len(peaks)
-    if n < 2:
-        return 0
-    dist = 0
+    max_dist = 0
+    avg_dist = 0
     for i in range(n - 1):
-        dist += (peaks[i + 1] - peaks[i]) / (n - 1)
-    return dist
+        d = (peaks[i + 1] - peaks[i])
+        avg_dist += d / (n - 1)
+        max_dist = max(max_dist, d)
+    return avg_dist, max_dist
+
+
+def get_fano_factor(signal):
+    return np.var(signal) / np.mean(signal, dtype=np.float64)
 
 
 def get_peak_features(article_ids, article_df, tweet_df_map):
@@ -175,26 +178,33 @@ def get_peak_features(article_ids, article_df, tweet_df_map):
     dr_col_map = {}
     active_col_map = {}
     npeaks_col_map = {}
-    peak_dist_col_map = {}
-    aid_bursty_map = defaultdict(list)
+    avg_peak_dist_col_map = {}
+    max_peak_dist_col_map = {}
+    # aid_bursty_map = defaultdict(list)
+    fano_factor_col_map = {}
+
     for aid in article_ids:
         tdf = tweet_df_map[aid]
-        ## dr[0] is the first share date
         dr = get_date_range(tdf)
         signal_df = get_signal_dataframe(tdf, dr)
         x = signal_df.tweet_count.to_numpy()
+        fano = get_fano_factor(x)
         peaks = get_signal_peaks(aid, x, graph=True)
-        # Map articles to "burstiness".
-        aid_bursty_map = get_exp_rates(aid_bursty_map, peaks, signal_df, x, aid) 
-        # Record the relevant features
+        # # Map articles to "burstiness".
+        # aid_bursty_map = get_exp_rates(aid_bursty_map, peaks, signal_df, x, aid)
+        # Save relevant peak data separately.
+        all_peaks.extend([(aid, dr[p], x[p]) for p in peaks])
+        # Record relevant lifespan features
         dr_col_map[aid] = len(dr) - 1   # Subtract extra day
         active_col_map[aid] = get_days_active(dr, signal_df)
         npeaks_col_map[aid] = len(peaks)
-        peak_dist_col_map[aid] = get_avg_peak_distance(peaks)
-        all_peaks.extend([(aid, dr[p], x[p]) for p in peaks])
-    # Draw and save bursty chart
-    save_rates_graph(aid_bursty_map)
-    
+        fano_factor_col_map[aid] = fano
+        # Record peak distance features
+        avg_dist, max_dist = get_peak_distance(peaks)
+        avg_peak_dist_col_map[aid] = avg_dist
+        max_peak_dist_col_map[aid] = max_dist
+    # # Draw and save bursty chart
+    # save_rates_graph(aid_bursty_map)
     # Save the collection of peaks found.
     all_peaks = np.transpose(all_peaks)
     pd.DataFrame({
@@ -202,12 +212,15 @@ def get_peak_features(article_ids, article_df, tweet_df_map):
         "peak_date": all_peaks[1],
         "peak_height": all_peaks[2],
     }).to_csv(f"{data_path}/article_peaks.csv")
-    # Save the articles 
+    # Save the articles
     article_df["lifespan"] = article_df['id'].map(dr_col_map)
     article_df["active_days"] = article_df['id'].map(active_col_map)
     article_df["num_peaks"] = article_df['id'].map(npeaks_col_map)
     article_df["active_ratio"] = article_df["active_days"] / article_df["lifespan"]
-    article_df["avg_peak_dist"] = article_df['id'].map(peak_dist_col_map)
+    article_df["fano_factor"] = article_df['id'].map(fano_factor_col_map)
+    article_df["avg_peak_dist"] = article_df['id'].map(avg_peak_dist_col_map)
+    article_df["max_peak_dist"] = article_df['id'].map(max_peak_dist_col_map)
+
 
 
 def main():
